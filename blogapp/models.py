283a3ad42190c6d86e1b2e4c960ecfff205af814
levi <15136161050@163.com>
2019-12-14 16:44:57 +0800

    创建model

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 # Create your models here.
+
+class TagModel(models.Model):
+    name = models.CharField(max_length=64,verbose_name='标签')
+    create_time = models.DateTimeField(default=now,verbose_name='创建时间')
+
+    def __str__(self):
+        return '标签名:{}'.format(self.name)
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = '标签名称'
+
+
+class BlogModel(models.Model):
+    user = models.ForeignKey(to=User,related_name='blogs',on_delete=models.CASCADE)
+    title = models.CharField(default='',max_length=64,verbose_name='标题')
+    content = models.TextField(default='',verbose_name='内容')
+    create_time = models.DateTimeField(default=now,verbose_name='创建时间')
+    tags = models.ManyToManyField(to=TagModel,verbose_name='标签',related_name='bolgs')
+
+    def __str__(self):
+        return '作者:{} 标题:{} 标签:{}'.format(self.user.username,self.title)
+
+    class Meta:
+        verbose_name = '博客'
+        verbose_name_plural = '博客名称'
--- modelcurl/settings.py
+++ modelcurl/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogapp',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,15 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
